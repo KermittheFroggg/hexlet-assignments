99c4ea0515b21d85d0394cd493c4bcce36770bb3
@hexlet/cli <support@hexlet.io>
2023-06-16 15:32:14 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -16,8 +16,8 @@ public class App {
         for (var i = 0; i < arraySentence.length; i++) {
             Integer count = 1;
             if (result.containsKey(arraySentence[i])) {
-            Integer count2 = result.get(arraySentence[i]) + 1;
-            result.put(arraySentence[i], count2);
+                Integer count2 = result.get(arraySentence[i]) + 1;
+                result.put(arraySentence[i], count2);
             } else {
                 result.put(arraySentence[i], count);
             }
@@ -27,12 +27,14 @@ public class App {
 
     public static String toString(Map numbersOfWord) {
         Map<String, Integer> numbersOfWord2 = numbersOfWord;
-        String result = "{\n";
-
+        if (numbersOfWord2.isEmpty()) {
+            return "{}";
+        }
+        String result = "{";
         for (Map.Entry<String, Integer> word: numbersOfWord2.entrySet()) {
             result = result + "\n  " + word.getKey() + ": " + word.getValue();
         }
-        return result + "}";
+        return result + "\n}";
     }
 }
 
