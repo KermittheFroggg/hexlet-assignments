da77de70fb22ba1d7d00475f4062eb7a75196915
@hexlet/cli <support@hexlet.io>
2023-06-16 15:22:34 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -10,24 +10,27 @@ public class App {
         String[] arraySentence = lowerSentence.split(" ");
         Map<String, Integer> result = new HashMap<>();
 
-        for (var i = 0; i < arraySentence.length; i++) {
-        Integer count = 1;
-        if (result.containsKey(arraySentence[i])) {
-        Integer count2 = result.get(arraySentence[i]) + 1;
-        result.put(arraySentence[i], count2);
-        } else {
-        result.put(arraySentence[i], count);
+        if (sentence.equals("")) {
+            return result;
         }
+        for (var i = 0; i < arraySentence.length; i++) {
+            Integer count = 1;
+            if (result.containsKey(arraySentence[i])) {
+            Integer count2 = result.get(arraySentence[i]) + 1;
+            result.put(arraySentence[i], count2);
+            } else {
+                result.put(arraySentence[i], count);
+            }
         }
         return result;
     }
 
-public static String toString(Map numbersOfWord) {
+    public static String toString(Map numbersOfWord) {
         Map<String, Integer> numbersOfWord2 = numbersOfWord;
         String result = "{\n";
 
         for (Map.Entry<String, Integer> word: numbersOfWord2.entrySet()) {
-        result = result + "  " + word.getKey() + ":" + word.getValue() + "\n";
+            result = result + "\n  " + word.getKey() + ": " + word.getValue();
         }
         return result + "}";
     }
