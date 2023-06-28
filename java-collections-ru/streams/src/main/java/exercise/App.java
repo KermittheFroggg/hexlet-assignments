585d5eefe3908029fa2d2bf16a22f37b75cf5361
@hexlet/cli <support@hexlet.io>
2023-06-28 17:44:30 +0300

    submit java-collections-ru/streams

--- java-collections-ru/streams/src/main/java/exercise/App.java
+++ java-collections-ru/streams/src/main/java/exercise/App.java
@@ -9,8 +9,8 @@ public class App {
         long count = emailsList.stream()
                 .filter(email -> StringUtils.isNotBlank(email))
                 .map(email -> email.split("@"))
-                .filter(email -> email[1].equals("gmail.com") ||
-                        email[1].equals("yandex.ru") || email[1].equals("hotmail.com"))
+                .filter(email -> email[1].equals("gmail.com")
+                        || email[1].equals("yandex.ru") || email[1].equals("hotmail.com"))
                 .count();
         return (int) count;
     }
